437d43e8fb8e06328ee79413c68905313e3123af
Mikkel Hartig Braunstein <mikk151g@edu.campusvejle.dk>
2021-01-12 09:48:29 +0100

    Remove initial data

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <the-icons></the-icons>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheIcons from "./TheIcons.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TheIcons,
+  },
+};
 </script>
