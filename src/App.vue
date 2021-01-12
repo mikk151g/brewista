aaef67249353f84762375cbe3e5dafbef20d2e13
Mikkel Hartig Braunstein <mikk151g@edu.campusvejle.dk>
2021-01-12 15:02:50 +0100

    Add CoffeeRecipies component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <the-homepage></the-homepage>
   <pick-coffee></pick-coffee>
+  <coffee-recipies></coffee-recipies>
 </template>
 
 <script>
 import TheHomepage from "./components/TheHomepage.vue";
 import PickCoffee from "./components/PickCoffee.vue";
+import CoffeeRecipies from "./components/CoffeeRecipies.vue";
 
 export default {
   components: {
     TheHomepage,
     PickCoffee,
+    CoffeeRecipies,
   },
 };
 </script>
---
+++ src/components/CoffeeRecipies.vue
@@ -0,0 +1,35 @@
+<template>
+  <the-icons
+    icon="arrowBack"
+    icon-classes="w-7 h-7 mr-4"
+    wrapper-classes="flex mx-4 my-8"
+    ><h1 class="font-semibold text-lg">How do you want to brew</h1></the-icons
+  >
+  <main class="m-4">
+    <section class="mb-16">
+      <h2>Favorite Recipe</h2>
+      <brew-card></brew-card>
+    </section>
+    <section class="my-8">
+      <h2>Try a new recipe</h2>
+      <brew-card><brew-card>
+    </section>
+  </main>
+</template>
+
+<script>
+import TheIcons from "./TheIcons.vue";
+import BrewCard from "./BrewCard.vue";
+export default {
+  components: {
+    TheIcons,
+    BrewCard,
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  @apply text-gray-400 uppercase;
+}
+</style>
