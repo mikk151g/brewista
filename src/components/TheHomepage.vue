9fbf0c7f4e548884f95c6efc2e9e629ccc70cb9f
Mikkel Hartig Braunstein <mikk151g@edu.campusvejle.dk>
2021-01-13 09:11:28 +0100

    Fix button

--- src/components/TheHomepage.vue
+++ src/components/TheHomepage.vue
@@ -26,7 +26,6 @@ export default {
 <style scoped>
 .brew-button {
   background: yellow;
-  @apply uppercase rounded-full font-bold px-14 py-6
-    fixed origin-center transform translate-x-1/3 bottom-5;
+  @apply uppercase rounded-full font-bold px-14 py-6 mx-auto fixed left-1/2 transform -translate-x-1/2 bottom-5;
 }
 </style>
